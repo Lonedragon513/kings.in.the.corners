dd91c27bebd7fbdda8893b8c37eb160978976c1c
Solus <lonedragon513@gmail.com>
2022-03-05 16:40:27 -0800

    initial

---
+++ src/main/java/org/jointheleague/api/king/KingsInTheCornersApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingsInTheCornersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingsInTheCornersApplication.class, args);
+	}
+
+}
